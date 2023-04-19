04582e352500b9266b1cd1198dc35c2e8032def6
Anke Van Humbeeck <anke.van.humbeeck@student.ehb.be>
2023-04-19 17:39:23 +0200

    weatherapp done

--- oefening2/index.js
+++ oefening2/index.js
@@ -1,24 +1,42 @@
 "use strict";
 
+let weather = [];
+
 const weatherApp = {
-  init: {
+	init() {
+		document.getElementById("form").addEventListener("submit", this.changeCity);
+	},
+	changeCity(e) {
+		//to clear html
 
-  },
-  fetchWeatherData(city) {
-
-  },
-  renderWeatherData(weatherData) {
-
-    //Template
-    /*
+		//to change name
+		console.log("click");
+		e.preventDefault();
+		const cityName = document.getElementById("search").value;
+		weatherApp.fetchWeatherData(cityName);
+		console.log(cityName);
+	},
+	fetchWeatherData(city) {
+		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=d7b955c4c268fe54649d6f0d702b39d1&units=metric`)
+			.then((response) => response.json())
+			.then(function (data) {
+				console.log(data);
+				weatherApp.renderWeatherData(data);
+			});
+	},
+	renderWeatherData(weatherData) {
+		const weather = `
     <h2>Weather</h2>
-        <ul>
-          <li>Average tempature: ${temp} °C</li>
-          <li>Min tempature: ${temp_min} °C</li>
-          <li>Max tempature: ${temp_max} °C</li>
-          <li>Description: ${description}</li>
-        </ul>
-    */
+      <ul>
+        <li>Average tempature: ${weatherData.main.temp} °C</li>
+        <li>Min tempature: ${weatherData.main.temp_min} °C</li>
+        <li>Max tempature: ${weatherData.main.temp_max} °C</li>
+        <li>Description: ${weatherData.weather[0].description}</li>
+      </ul>
+    `;
 
-  }
-}
+		document.querySelector("#weatherResult").insertAdjacentHTML("afterbegin", weather);
+	},
+};
+
+weatherApp.init();
